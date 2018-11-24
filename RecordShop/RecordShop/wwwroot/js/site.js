36f9b9cc898eae7dcb01cd2cdc869229e0a7266c
Jon Wendt <jmwendt7@gmail.com>
2018-11-24 17:22:19 -0600

    Seeing if this works

--- RecordShop/RecordShop/wwwroot/js/site.js
+++ RecordShop/RecordShop/wwwroot/js/site.js
@@ -15,5 +15,56 @@ angular.module('record', []).controller('SiteCtrl', ['$scope', function ($scope)
         slidesToShow: 5,
         slidesToScroll: 5,
     });
+
+    $scope.listOfAlbums = [
+        {
+            name: "Album1",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album2",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album3",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album4",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album5",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album6",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album7",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album8",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album9",
+            img: "Not a link yet, because I don't know how to just yet."
+        },
+        {
+            name: "Album10",
+            img: "Not a link yet, because I don't know how to just yet."
+        }
+        ];
+
+    $scope.album = {
+        name: "Abbey Road",
+        imgSrc: "~/images/AbbeyRoad.jpg",
+        distributer: "That place down the road",
+        description: "This is an album that is good and has an iconic album cover. Get it before it runs out!",
+        price: 29.99
+    };
 }]);
 
