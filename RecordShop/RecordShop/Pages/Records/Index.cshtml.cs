95bec79c3657cb87d87bea2ee6d798bdbff9f6fd
Bobby <bomarkim3@gmail.com>
2018-12-05 06:02:55 -0600

    Updated Nav bar to return proper index, Implemented search feature to return products (lambda functions) Next: Shopping Cart, Validation(easy), Sign-Ins? and CRUD functions (should get done first)

--- RecordShop/RecordShop/Pages/Records/Index.cshtml.cs
+++ RecordShop/RecordShop/Pages/Records/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RecordShop.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RecordShop.Records
 {
@@ -19,10 +20,34 @@ namespace RecordShop.Records
         }
 
         public IList<Record> Record { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        //Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string RecordGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Record = await _context.Record.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Record
+                                               orderby m.Genre
+                                               select m.Genre;
+
+            var records = from m in _context.Record
+                          select m;
+
+            if(!String.IsNullOrEmpty(SearchString))
+            {
+                records = records.Where(s => s.Name.Contains(SearchString));
+            }
+
+            if (!String .IsNullOrEmpty(RecordGenre))
+            {
+                records = records.Where(x => x.Genre == RecordGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Record = await records.ToListAsync();
         }
     }
 }
